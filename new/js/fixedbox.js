0eb299c6ad9c706f7e4960359166e7a4db461081
Andy126 <xiyoukeji@xiyoukejideMacBook-Pro.local>
2018-11-26 15:32:16 +0800

    tb

---
+++ new/js/fixedbox.js
@@ -0,0 +1,14 @@
+// var screenWidth = document.body.clientWidth;
+// if (screenWidth < 768) {
+
+// }
+// if (document.getElementById('phone-box')) {
+//     var PB = document.getElementById('phone-box')
+//     PB.onclick = function(e) {
+//         console.log(1)
+//         e.preventDefault()
+//         PB.style.width = 180 + 'px'
+//     }
+// } else {
+//     console.log('false')
+// }
